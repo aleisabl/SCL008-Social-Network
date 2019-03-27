f18606184715e4546eba8a36ff95f1f5f9493fd8
aleisabl <aleisabl.25@gmail.com>
2019-03-27 17:25:25 -0300

    borrando prueba de firebase, agregando inicializacion de firebase definitiva

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 //llamado a la iniciación de la ruta 
-//inicializando firebase
+// Initialize Firebase
+
+ var config = {
+    apiKey: "AIzaSyBhGWWeKj-GpQV7JzujZCE-fzms0LoZgZ4",
+    authDomain: "social-network-682a3.firebaseapp.com",
+    databaseURL: "https://social-network-682a3.firebaseio.com",
+    projectId: "social-network-682a3",
+    storageBucket: "social-network-682a3.appspot.com",
+    messagingSenderId: "307635345259"
+  };
+  firebase.initializeApp(config);
 
 import { initRouter } from './route.js';
 
