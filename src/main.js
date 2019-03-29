0684f2e1e79e6529d6a953636e1b774c117c9e2f
aleisabl <aleisabl.25@gmail.com>
2019-03-29 13:35:44 -0300

    agregando lo que falto de firebase

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 //llamado a la iniciación de la ruta 
 // Initialize Firebase
 
- var config = {
-    apiKey: "AIzaSyBhGWWeKj-GpQV7JzujZCE-fzms0LoZgZ4",
-    authDomain: "social-network-682a3.firebaseapp.com",
-    databaseURL: "https://social-network-682a3.firebaseio.com",
-    projectId: "social-network-682a3",
-    storageBucket: "social-network-682a3.appspot.com",
-    messagingSenderId: "307635345259"
-  };
-  firebase.initializeApp(config);
-
 import { initRouter } from './route.js';
 
 const init = () => {
