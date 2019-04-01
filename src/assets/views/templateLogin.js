e67a107f4237c426118d96626c8d1cbe4bd2ef3f
aleisabl <aleisabl.25@gmail.com>
2019-04-01 17:55:32 -0300

    probando tests

--- src/assets/views/templateLogin.js
+++ src/assets/views/templateLogin.js
@@ -59,3 +59,13 @@ export const templateLogin = () => {
     })
     return containerLogin;
 }
+
+export const hello = () => 'hello';
+
+export const add = (x, y) => {
+    if(typeof x != 'number' || typeof y != 'number'){
+        return null;
+    }
+
+    return x + y;
+}
