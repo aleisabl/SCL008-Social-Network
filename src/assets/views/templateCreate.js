3098321e72fe1ef76f99cb325774cde4533e223a
aleisabl <aleisabl.25@gmail.com>
2019-04-01 15:14:24 -0300

    cambiando diseño a mobile first

--- src/assets/views/templateCreate.js
+++ src/assets/views/templateCreate.js
@@ -3,67 +3,7 @@ import { createAccount } from './../js/auth.js';
 export const templateCreate = () => {
     //creamos div que contendrá la plantilla
     const containerCreate = document.createElement('div');
-    const contentCreate = `<div class='some-page-wrapper'>
-      <div class='row'>
-        <div class='column'>
-          <div class='blue-column'>
-            
-
-            <div>
-                <div class="carousel">
-                    <ul class="slides">
-                        <input type="radio" name="radio-buttons" id="img-1" checked />
-                        <li class="slide-container">
-                            <div class="slide-image">
-                                <img src="assets/Moodboard/prueba.png">
-                            </div>
-                            <div class="carousel-controls">
-                                <label for="img-3" class="prev-slide">
-                                    <span>&lsaquo;</span>
-                                </label>
-                                <label for="img-2" class="next-slide">
-                                  <span>&rsaquo;</span>
-                                </label>
-                            </div>
-                        </li>
-                        <input type="radio" name="radio-buttons" id="img-2" />
-                        <li class="slide-container">
-                            <div class="slide-image">
-                                <img src="assets/Moodboard/SEGUNDO.png">
-                            </div>
-                            <div class="carousel-controls">
-                                <label for="img-1" class="prev-slide">
-                                    <span>&lsaquo;</span>
-                                </label>
-                                <label for="img-3" class="next-slide">
-                                    <span>&rsaquo;</span>
-                                </label>
-                            </div>
-                        </li>
-                        <input type="radio" name="radio-buttons" id="img-3" />
-                        <li class="slide-container">
-                            <div class="slide-image">
-                                <img src="https://speakzeasy.files.wordpress.com/2015/05/twa_blogpic_timisoara-4415.jpg">
-                            </div>
-                            <div class="carousel-controls">
-                                <label for="img-2" class="prev-slide">
-                                    <span>&lsaquo;</span>
-                                </label>
-                                <label for="img-1" class="next-slide">
-                                    <span>&rsaquo;</span>
-                                </label>
-                            </div>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </div>
-
-
-
-
-          </div>
-        </div>
+    const contentCreate = `
         <div class='column'>
           <div class='green-column' id="green-columns">
             <div id="logo-welcome" >
@@ -88,9 +28,7 @@ export const templateCreate = () => {
             </div>
             <div >
                 <button id="create" class="sign-in-style">Crear cuenta</button>
-            </div>
-          
-    
+            </div>    
    `;
 
     //pasar cel contenido al div
