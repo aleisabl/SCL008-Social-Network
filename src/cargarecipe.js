9009b6e416428c95cd425a13dabd13603f3ad7f4
BlancaCarrasco <blankcarr@gmail.com>
2019-04-07 10:13:33 -0400

    prueba de archivos

---
+++ src/cargarecipe.js
@@ -0,0 +1,55 @@
+// const cargarRecipe= ()=>{
+// <div>
+// <section id="agregar">
+//     <div id="logIn" class="logIn">
+//         <div class="container">
+//             <div class="row menu">
+//                 <div class="col-1"></div>
+//                     <div class="col-10">
+//                         <div class="row">
+//                             <div class="col-12">
+//                                 <div class="post">
+//                                     <p>
+//                                         ¿Quieres publicar una receta?
+//                                     </p>
+//                                         <form id="sign-in" class="container">
+//                                             <div id="nombre">
+//                                             <p>Nombre de tu receta</p>
+//                                             <input id="titleRecipe" type="text">
+//                                             <p>Tiempo de Preparación</p>
+//                                             <input id="time" type="text">
+//                                             </div>
+//                                             <p >Ingredientes</p>
+//                                             <textarea id="Ingredients" name="Ingredients" cols="30" rows="10"></textarea>
+                                            
+//                                             <div  id="prep" class="form directions">
+//                                             <p id="preRec">Preparacion de tu receta</p>
+//                                             <textarea id="instRecipes" name="" cols="30" rows="10"></textarea>
+//                                             </div>
+//                                             <div class="form upload">
+//                                             <p id="picture">Imagen</p>
+//                                             <progress value="0" max="80" id="uploader">0%</progress><br>
+//                                             <br> <input type="file" value="upload" id="fileButton" name="elegir archivo" /></br>
+//                                             <button id="sendRecipe">Enviar Receta</button>
+//                                             </div>
+//                                         </form>
+//                                     <button id="postBtn" class="btn">Publicar</button>
+//                                 </div>
+//                             </div>
+//                         </div>                       
+//                         <div class="row">
+//                             <div class="col-12">
+//                                 <div id="contenedor"></div>
+//                                 <div id="postear" class="postear">
+//                                 </div>
+//                             </div>
+//                         </div>            
+//                     </div>
+//                     <div class="col-1">     
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+// </section>
+// </div>
+// }
--- src/main.js
+++ src/main.js
@@ -19,38 +19,43 @@ const init = () => {
 
 window.addEventListener('load', init);
 const post = () => {
-    let postEmpty = document.getElementById("postAlert");
-    let postText = document.getElementById("postText").value;
     if(postText === ""){
       postEmpty.style.display= "block";
     }
     else {
       postEmpty.style.display= "none";
-      let postText = document.getElementById("postText").value;
-      let userName = document.getElementById("postName").value;
       savePost(postText, userName);
     }
   } 
-  document.getElementById("postBtn").addEventListener("click", post);
+
 
   
  document.getElementById("hola").addEventListener("click",function(){
   document.getElementById("root").style.display="none";
-  document.getElementById("muro").style.display="block";
-    alert("hola")
-
-    
+  document.getElementById("agregar").style.display="block";
+  
+    // alert("hola") 
   });
+  
+  document.getElementById("entrar").addEventListener("click",function(){
+    document.getElementById("root").style.display="none";
+    document.getElementById("agregar").style.display="none";
+      // alert("como estas")
+    document.getElementById("page2").style.display="block";
+      
+    });
 
   let recipeKey = "";
-const recipe = (nameRecipe,newTime) => {
+const recipe = (nameRecipe,newTime,newingredients,newpreparation) => {
   recipeKey = firebase.database().ref('recipe/').child('likes').push().key;
 
   firebase.database().ref(`recipe/${recipeKey}`).set({
     likesCount : 0,
     titulo: nameRecipe,
     Key: recipeKey,
-    timePrep:newTime
+    timePrep:newTime,
+    ingredients:newingredients,
+    preparation:newpreparation
    
   });
   console.log("holaaaa")
@@ -60,9 +65,11 @@ const recipe = (nameRecipe,newTime) => {
 const saveRecipe = ()=>{
 const nameRecipe= titleRecipe.value;
 const newTime= time.value;
+const newingredients= Ingredients.value
+const newpreparation= instRecipes.value
 // console.log(newTime)
 
-recipe(nameRecipe,newTime)
+recipe(nameRecipe,newTime,newingredients,newpreparation)
 
 }
 sendRecipe.addEventListener('click', saveRecipe )
